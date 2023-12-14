67c3e4c29f937885611aac262fe83ff7bd1c207f
Jae BIN Lee <jeabin1129@naver.com>
2023-12-14 18:49:35 +0900

    Create HelloJava2.java

---
+++ src/HelloJava2.java
@@ -0,0 +1,11 @@
+public class HelloJava2 {
+    public static void main(String[] args){
+        System.out.println("hello java1");
+        System.out.println("hello java1");
+        System.out.println("hello java1");
+    }
+
+    public static void main2(String[] args) {
+        System.out.println();
+    }
+}
